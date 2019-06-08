0d3ff942037f430a894ee5ee48a5c9e79ea58e97
EricaMcc <emccul13@gmail.com>
2019-06-07 20:12:59 -0500

    showing user other users that match their humor type

--- client/src/components/match.jsx
+++ client/src/components/match.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserThumbPreview from './user-thumb-preview.jsx';
 
-
 function Match(props) {
-  const { user, allUsers, handleClick, changeView } = props;
-  // const { name, profilePicURL } = user;
+  const { user, userMatches, changeView, interests } = props;
+  const { displayName, profilePicURL, bio } = user;
 
-  // function handleClick(e) {
-  //   changeView(e.target.dataset.target, user);
-  // }
+  const [match, setMatch] = useState(userMatches[0]);
 
-  // useEffect(() => {
-  //   fetchConvo();
-  //   lastMessageRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-  // }, [conversation.length]);
+  function handleClick(e) {
+    changeView(e.target.dataset.target, match);
+  }
 
-  function fetchMatch() {
-    axios.get(`api/message/${user.id},${mainViewUser.id}`).then((response) => {
-      setConversation(response.data);
-    }).catch(() => {
-      // console.error(err);
-    });
+  function nextMatch(e) {
+    const index = userMatches.indexOf(match);
+    if (index + 1 < userMatches.length) {
+      setMatch(userMatches[index + 1]);
+    }
   }
 
   return (
-    <div className="d-flex flex-row justify-content-between flex-wrap">
-      {allUsers.filter(u => u.username !== user.username)
-        .map(u => <UserThumbPreview key={u.username} user={u} changeView={changeView} />)}
+    <div className="mx-2 mb-3">
+      <div className="d-flex flex-column">
+        <div data-target="profile" onClick={handleClick}>{match.displayName}</div>
+        <img data-target="profile" className="img-thumbnail img-sm" src={match.profilePicURL} onClick={handleClick} />
+        <div className="d-flex flex-row">
+          <button data-target="dm" type="button" className="btn btn-sm btn-success" onClick={handleClick}>slide into their DM </button>
+          <button data-target="profile" type="button" className="btn btn-sm btn-success" onClick={nextMatch}>Show me another</button>
+        </div>
+        <div>
+          <p>{match.bio}</p>
+          <ul>
+            {interests.map((interest) => {
+              <li>interest</li>
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
-    // <div className="mx-2 mb-3">
-    //   <div className="d-flex flex-column">
-    //     <div data-target="profile" onClick={handleClick}>{name}</div>
-    //     <img data-target="profile" className="img-thumbnail img-sm" src={profilePicURL} onClick={handleClick} />
-    //     <button data-target="dm" type="button" className="btn btn-sm btn-success" onClick={handleClick}>slide into their DM </button>
-    //   </div>
-    // </div>
   );
 }
+
+  // return (
+  //   <div className="d-flex flex-row justify-content-between flex-wrap">
+  //     {allUsers.filter(u => u.username !== user.username)
+  //       .map(u => <UserThumbPreview key={u.username} user={u} changeView={changeView} />)}
+  //   </div>
+  // );
+// }
 
 export default Match;
--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function Profile(props) {
   const { mainViewUser, changeView } = props;
   const {
-    username, name, profilePicURL, interests, bio,
+    username, displayName, profilePicURL, interests, bio,
   } = mainViewUser;
 
   function handleClick(e) {
@@ -23,7 +23,7 @@ function Profile(props) {
 
         <h3>{name}</h3>
         <button data-target="dm" type="button" className="btn btn-success" onClick={handleClick}>
-          slide into {name}'s DM
+          slide into {displayName}'s DM
         </button>
         <div className="mt-2">{bio}</div>
       </div>
